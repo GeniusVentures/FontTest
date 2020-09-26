973790c0071515a255d0153afe1dbc101d6fff05
Kenneth Hurley <ken@geniusventures.io>
2020-09-25 22:22:30 -0700

    Generated code from sketch that can use and display different fonts.

---
+++ lib/screens/fonttest/desktop.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+class Desktop extends StatefulWidget{
+	const Desktop() : super();
+@override
+_Desktop createState() => _Desktop();
+}
+
+class _Desktop extends State<Desktop>{
+
+
+@override
+Widget build(BuildContext context){
+return Scaffold(
+body: Align(alignment: Alignment(0.29, -0.35),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.903,child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+children: [Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 100 Thin',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w100,
+fontStyle: FontStyle.normal,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 100 Thin Italic',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w100,
+fontStyle: FontStyle.italic,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Spacer(flex: 3,),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 200 Extra Light',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w200,
+fontStyle: FontStyle.normal,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 200 Extra Light Italic',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w200,
+fontStyle: FontStyle.italic,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Spacer(flex: 3,),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 300 Light',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w300,
+fontStyle: FontStyle.normal,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 300 Light Italic',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w300,
+fontStyle: FontStyle.italic,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Spacer(flex: 3,),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 400 Regular',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w400,
+fontStyle: FontStyle.normal,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 400 Italic',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w400,
+fontStyle: FontStyle.italic,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Spacer(flex: 3,),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 500 Medium',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w400,
+fontStyle: FontStyle.normal,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 500 Medium Italic',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w400,
+fontStyle: FontStyle.italic,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Spacer(flex: 3,),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 600 Semi Bold',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w600,
+fontStyle: FontStyle.normal,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 600 Semi Bold Italic',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w600,
+fontStyle: FontStyle.italic,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Spacer(flex: 3,),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 700 Bold',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w700,
+fontStyle: FontStyle.normal,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 700 Bold Italic',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w700,
+fontStyle: FontStyle.italic,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Spacer(flex: 3,),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Stack(
+children: [Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 800 Extra Bold',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w800,
+fontStyle: FontStyle.normal,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),)),Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 800 Extra Bold',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w800,
+fontStyle: FontStyle.normal,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),)),]))),),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Stack(
+children: [Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 800 Extra Bold Italic',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w800,
+fontStyle: FontStyle.italic,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),)),Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 800 Extra Bold Italic',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w800,
+fontStyle: FontStyle.italic,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),)),]))),),Spacer(flex: 3,),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 900 Black',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w900,
+fontStyle: FontStyle.normal,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),Flexible(flex: 5,child: Padding(padding: EdgeInsets.only(),child: Container(width : MediaQuery.of(context).size.width * 0.954, height : MediaQuery.of(context).size.height * 0.042,child: Align(alignment: Alignment(0.00, 0.00),child: Text(
+'Prompt Font Weight 900 Black Italic',
+style: TextStyle(
+fontSize: 24,
+fontWeight: FontWeight.w900,
+fontStyle: FontStyle.italic,
+color: Colors.black,),textAlign: TextAlign.center,
+
+),))),),])),), 
+);
+}
+}
